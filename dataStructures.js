a56adc5e3fd4fed3efb7085bbc1da836a469d933
Maciej Mucha <mmucha.00@gmail.com>
2019-06-08 12:24:15 +0200

    Initial commit

---
+++ dataStructures.js
@@ -0,0 +1,89 @@
+class FluidType {
+    constructor(index, color, mass, renderRadius, isMoveable) {
+        this.index = index;
+        this.color = color;
+        this.mass = mass;
+        this.renderRadius = renderRadius;
+        this.isMoveable = isMoveable;	// szklanka to też typ płyn tylko bez możliwości przemieszczania
+    }
+}
+  
+class Vector3 {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    getTreeVector() {
+        return new THREE.Vector3( this.x, this.y , this.z );
+    }
+}
+
+class Particle {
+    constructor(r, fluidTypeIndex) {
+        this.R = r; // położenie
+        this.fluidTypeIndex = fluidTypeIndex;
+
+        self.V = new Vector3(0, 0, 0);    	// prędkość
+        self.A = new Vector3(0, 0, 0);    // przyspieszenie
+        self.g = 0;   // gęstość
+        self.p = 0;   // ciśnienie
+        self.cell = Cell.from(r)  // komórka w której sie znajduje -> można ją dostać z położenia w czasie stałym (!)
+    }
+}
+function* getNeighboursList(particlePosition)	{// generator dający wszystkie sząstki z sąsiednich komórek
+
+}
+  
+class Cell {
+    constructor(offsetX, offsetY, offsetZ) {
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        this.offsetZ = offsetZ;
+        this.particles = [];
+    }
+
+    getMyCellIndex() {
+    }
+
+    getMyCellOffsetX() {
+    }
+    getMyCellOffsetY() {
+    }
+    getMyCellOffsetZ() {
+    }
+
+}
+  
+class Fluid {
+    constructor(sizeX, sizeY, sizeZ, cellLength) {
+        this.particles = [];
+
+        let numOfCells = Math.ceil(sizeX / cellLength) * Math.ceil(sizeY / cellLength) * Math.ceil(sizeZ / cellLength);
+        this.cells = new Array(numOfCells);
+
+    }
+}
+
+function getZindex(x, y, z) {
+    return mortonEncode_magicbits(x, y, z);
+}
+
+// source:    https://www.forceflow.be/2013/10/07/morton-encodingdecoding-through-bit-interleaving-implementations/
+// method to seperate bits from a given integer 3 positions apart
+function splitBy3(a) {
+    var x = a & 0x1fffff; // we only look at the first 21 bits
+    x = (x | x << 32) & 0x1f00000000ffff; // shift left 32 bits, OR with self, and 00011111000000000000000000000000000000001111111111111111
+    x = (x | x << 16) & 0x1f0000ff0000ff; // shift left 32 bits, OR with self, and 00011111000000000000000011111111000000000000000011111111
+    x = (x | x << 8) & 0x100f00f00f00f00f; // shift left 32 bits, OR with self, and 0001000000001111000000001111000000001111000000001111000000000000
+    x = (x | x << 4) & 0x10c30c30c30c30c3; // shift left 32 bits, OR with self, and 0001000011000011000011000011000011000011000011000011000100000000
+    x = (x | x << 2) & 0x1249249249249249;
+    return x;
+}
+    
+function mortonEncode_magicbits(x, y, z) {
+    var answer = 0;
+    answer |= splitBy3(x) | splitBy3(y) << 1 | splitBy3(z) << 2;
+    return answer;
+}
