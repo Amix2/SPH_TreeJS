26ba071f85fa65d68a31ab5e601fa58d270b326e
Shino1024 <justincompspec@gmail.com>
2019-06-09 23:53:53 +0200

    first formulae implementation version

---
+++ sph.js
@@ -0,0 +1,106 @@
+class SPH {
+
+    // 1
+    static calcMass(g0, V, N) {
+        return g0 * V / N;
+    }
+
+    // 2
+    static calcVectorDiff(ri, rj) {
+        return Math.sqrt(Math.pow(ri.x - rj.x, 2) + Math.pow(ri.y - rj.y, 2) + Math.pow(ri.z - rj.z, 2));
+    }
+
+    // 3
+    static calcKernel(q, h) {
+        if (0 < q < h) {
+            return 3 / (2 * Math.PI) * (2 / 3 - q * q + q * q * q / 2);
+        } else if (h < q < 2 * h) {
+            return 3 / (2 * Math.PI) * (Math.pow(2 - q, 3) / 6);
+        } else if (2 * h < q) {
+            return 0;
+        }
+    }
+
+    static calcKernelDerivative(q, h) {
+        if (0 < q < h) {
+            return 3 / (2 * Math.PI) * (2 / 3 - 2 * q + 3 * q * q / 2);
+        } else if (h < q < 2 * h) {
+            return 3 / (2 * Math.PI) * (-3) * (Math.pow(2 - q, 2) / 6);
+        } else if (2 * h < q) {
+            return 0;
+        }
+    }
+
+    static calcKernelSecondDerivative(q, h) {
+        if (0 < q < h) {
+            return 3 / (2 * Math.PI) * (2 / 3 - 2 + 6 * q / 2);
+        } else if (h < q < 2 * h) {
+            return 3 / (2 * Math.PI) * 6 * (2 - q) / 6;
+        } else if (2 * h < q) {
+            return 0;
+        }
+    }
+
+    // 4
+    static calcDensity(mj, q, h, j, d = 3) {
+        let g = 0;
+        for (let i = 0; i < j; ++i) {
+            g += this.calcKernel(q[i][j], h);
+        }
+
+        return mj * g / Math.pow(h, d);
+    }
+
+    // 5
+    static calcPressure(k, gi, g0) {
+        return k * (Math.pow(gi / g0, 7) - 1);
+    }
+
+    // 6
+    static calcPressureVector(mj, gj, gi, pj, qj, p, q, rj) {
+        let a = 0;
+        for (let i = 0; i < j; ++i) {
+            a += (p[i] / q[i] / q[i] + pj / qj / qj) * this.calcKernelDerivative(rj[i])
+        }
+        return mj * a / gj / gi;
+    }
+
+    // 7
+    static calcViscosityVector(mj, gj, gi, vj, qj, v, q, rj, theta) {
+        let a = 0;
+        for (let i = 0; i < j; ++i) {
+            a += (v[i] / q[i] / q[i] + vj / qj / qj) * this.calcKernelSecondDerivative(rj[i])
+        }
+        return theta * mj * a / gj / gi;
+    }
+
+    // 8
+    static calcAOther(G) {
+        return G;
+    }
+
+    // 9
+    static calcAVector(mj, gj, vj, qj, gi, v, q, p, pj, theta, rj, G) {
+        return this.calcPressureVector(mj, gj, gi, pj, qj, p, q, rj) +
+            this.calcViscosityVector(mj, gj, gi, vj, qj, v, q, rj) +
+            this.calcAOther(G)
+    }
+
+    // 10
+    static calcVelocityHalfDelta(vi, t, deltaT, ai) {
+        let v = new Vector3();
+        v.x = vi.x * (t - deltaT / 2) + deltaT * ai(t).x;
+        v.y = vi.y * (t - deltaT / 2) + deltaT * ai(t).y;
+        v.z = vi.z * (t - deltaT / 2) + deltaT * ai(t).z;
+        return v;
+    }
+
+    // 11
+    static calcMovementDelta(xi, t, deltaT, vi) {
+        let x = new Vector3();
+        x.x = xi(t).x + vi.x * (t + deltaT / 2) * deltaT;
+        x.y = xi(t).y + vi.y * (t + deltaT / 2) * deltaT;
+        x.z = xi(t).z + vi.z * (t + deltaT / 2) * deltaT;
+        return x;
+    }
+}
