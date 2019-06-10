48f164a58346093b51cbdaa4e6da3c751d4503c7
Szymon Czarny <sszymek.5@gmail.com>
2019-06-10 17:42:46 +0200

    weaks

--- particleobject.js
+++ particleobject.js
@@ -8,40 +8,43 @@ class ParticleObject{
         for(var particleGeometry of particleGeometries)
             particles = particles.concat(particleGeometry.generateParticles(density, fluidIndex));
         
-        for(var i = 0; i < particles.length; i++)
-            particles[i].R = particles[i].R.add(this.position);
+        
+        for(var i = 0; i < particles.length; i++){
+           // console.log(particles[i]);
+            particles[i].R.add(position);
+        }
         this.particles = particles;
     }
 
     move(vector){
         this.position = this.position.add(vector);
         for(var i = 0; i < this.particles.length; i++)
-            this.particles[i] = this.particles[i].R.add(this.position);
+            this.particles[i].R.add(this.position);
     }
 
     rotatateAxis(axis, value){
         for(var i = 0; i < this.particles.length; i++) {
-            this.particles[i] = this.particles[i].R.add(this.position.multiplyScalar(-1));
+            this.particles[i].R.sub(this.position);
             this.particles[i].position.applyAxisAngle(axis, value);
-            this.particles[i] = this.particles[i].R.add(this.position);
+            this.particles[i].R.add(this.position);
           }
     }
 }
 
 class ParticleMug extends ParticleObject {
-    constructor(position, density, fluidIndex, radius, height, thickness){
+    constructor(position, gap, fluidIndex, radius, height, thickness){
         var mugGeometries = [];
-        mugGeometries.push(new CyllinderParticleGeometry(position, radius, height, false)); //outer cyllinder
-        mugGeometries.push(new CircleParticleGeometry(position - (height / 2.0) - density, 0, radius)); //outer bottom cap
+        mugGeometries.push(new CyllinderParticleGeometry(new THREE.Vector3(), radius, height)); //outer cyllinder
+        mugGeometries.push(new CircleParticleGeometry(new THREE.Vector3().sub(new THREE.Vector3(0, height * 0.5 + gap, 0)), radius)); //outer bottom cap
         if(thickness > 0){
             var innerCylPos = position.add(new THREE.Vector3(0, thickness / 2.0, 0));
             var innerCylH = height - thickness;
             var innerCylR = radius - thickness;
-            mugGeometries.push(new CyllinderParticleGeometry(innerCylPos, innerCylR, innerCylH, false)); //inner cyllinder
-            mugGeometries.push(new CircleParticleGeometry(innerCylPos - (innerCylH / 2.0) - density, 0, innerCylR)); //inner cyllinder
-            mugGeometries.push(new CircleParticleGeometry(position + (height / 2.0) + density, innerCylR, radius)); // top rim
+            mugGeometries.push(new CyllinderParticleGeometry(innerCylPos, innerCylR, innerCylH)); //inner cyllinder
+            mugGeometries.push(new CircleParticleGeometry(innerCylPos - (innerCylH / 2.0) - gap, innerCylR)); //inner cyllinder
+  //          mugGeometries.push(new CircleParticleGeometry(position + (height / 2.0) + gap, innerCylR, radius)); // top rim
         }
-            super(mugGeometries, position, density, fluidIndex);        
+        super(mugGeometries, position, gap, fluidIndex);        
         this.radius = radius;
         this.height = height;
         this.thickness = thickness;
@@ -54,7 +57,7 @@ class ParticleGeometry{
             throw new TypeError('Abstract class ParticleGeometry cannot be instantiated directly.'); 
         }
         if (this.generateParticles === undefined) {
-            throw new TypeError('Classes extending the ParticleGeometry abstract class must implement generateParticles(density: number) function');
+            throw new TypeError('Classes extending the ParticleGeometry abstra  ct class must implement generateParticles(density: number) function');
         }
 
         this.position = position;
@@ -64,7 +67,7 @@ class ParticleGeometry{
 }
 
 class CyllinderParticleGeometry extends ParticleGeometry{
-    constructor(position, radius, height, capped){
+    constructor(position, radius, height, capped = false){
         super(position);
         this.radius = radius;
         this.height = height;
@@ -73,20 +76,21 @@ class CyllinderParticleGeometry extends ParticleGeometry{
 
     generateParticles(gap, fluidIndex){
         var particles = [];
-        var layerPosition = new THREE.Vector3(0, this.position - this.height / 2.0, 0);
-        for(var i = 0; i <= this.height; i += gap){
+        var layerPosition = new THREE.Vector3(0, this.position.y - this.height * 0.5, 0);
+        for(let i = 0; i <= this.height; i += gap){
             if((i === 0 || i + gap > this.height) && this.capped === true)
-                particles = particles.concat(new CircleParticleGeometry(layerPosition, 0, this.radius).generateParticles(gap, fluidIndex));
+                particles = particles.concat(new CircleParticleGeometry(layerPosition, this.radius).generateParticles(gap, fluidIndex));
             else
-                particles = particles.concat(new CircleParticleGeometry(layerPosition, this.radius, this.radius).generateParticles(gap, fluidIndex));
+                particles = particles.concat(new CircleParticleGeometry(layerPosition, this.radius, true).generateParticles(gap, fluidIndex));
             layerPosition.add(new THREE.Vector3(0, gap, 0));
         }
+        console.log(particles);
         return particles;
     }
 }
 
 class CircleParticleGeometry extends ParticleGeometry{
-    constructor(position, radius, isRing){
+    constructor(position, radius, isRing = false){
         super(position);
         this.radius = radius;
         this.isRing = isRing;
@@ -94,13 +98,42 @@ class CircleParticleGeometry extends ParticleGeometry{
 
     generateParticles(gap, fluidIndex){
         var particles = [];
+        const rotationAxis = new THREE.Vector3(0, 1, 0);
+        const eps = 0.001;
 
-        if(isRing === false){
-            let faceLength = 0;
-            while()
+        if(this.isRing === false){
+            const moveOut = new THREE.Vector3(-gap * 0.5, 0, -gap * Math.sqrt(3) * 0.5);
+            const amountOfFaces = 6;
+
+            var cursor = new THREE.Vector3();
+            var moveDirection = new THREE.Vector3(gap, 0, 0);
+
+            particles.push(new Particle(new THREE.Vector3().add(this.position), fluidIndex));
+            cursor.add(moveOut);
+            var currentFaceLength = 1;
+            while(currentFaceLength * gap * Math.sqrt(3) * 0.5 <= this.radius + eps){
+                for(let i = 0; i < amountOfFaces; i++){
+                    for(let j = 0; j < currentFaceLength; j++){
+                        if(cursor.length() <= this.radius + eps){
+                            particles.push(new Particle(new THREE.Vector3().add(cursor).add(this.position), fluidIndex));
+                        }
+                        cursor.add(moveDirection);
+                    }
+                    moveDirection.applyAxisAngle(new THREE.Vector3(0, 1, 0), -Math.PI/3);
+                }
+                cursor.add(moveOut);
+                currentFaceLength++; 
+            }
         }
         else{
-
+            var cursor = new THREE.Vector3(this.radius, 0, 0);
+            const amount = Math.ceil((2 * Math.PI) / Math.acos(1 - (Math.pow(gap, 2) * 0.5 / Math.pow(this.radius, 2))));
+            console.log(amount)
+            const angle = (2 * Math.PI) / amount;
+            for(let i = 0; i < amount; i++){
+                particles.push(new Particle(new THREE.Vector3().add(cursor).add(this.position), fluidIndex));
+                cursor.applyAxisAngle(rotationAxis, angle);
+            }
         }
         return particles;
     }
